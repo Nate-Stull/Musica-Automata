9c9fec870a07c685d1e4b7131b35d65367cf31d0
Nate-Stull <nt.copf@gmail.com>
2019-04-01 15:50:43 -0400

    Essentially finished; still some slowdown after running for a little bit

--- src/Automata.java
+++ src/Automata.java
@@ -3,90 +3,130 @@ import java.util.concurrent.TimeUnit;
 
 public class Automata {
 
-    private String rule;
-    private String[] curGen = new String[171];
-
     public static void main (String args[]) {
-        new Automata(30);
-    }
+        char[] curGen = new char[101];
 
-    public Automata(int rule){
-        this.rule = getBinaryString(rule);
-
-        curGen [curGen.length/2] = "1";
-        for(int i = 0; i < curGen.length; i++) {
-            if(curGen[i] == null) curGen[i] = "0";
+        curGen [curGen.length/2] = '1';//all generations start with array of 0's and 1 in middle
+        for(int i = 0; i < curGen.length; i++) {//populating rest of array with 0's
+            if(curGen[i] != '1') curGen[i] = '0';
         }
 
-        generate();
+        new Automata(30, new StringBuilder(), new StringBuilder(), new StringBuilder(), curGen);
     }
 
-    public void generate() {
-        String binaryNum = "";
+    public Automata(int intRule, StringBuilder fourBitNum, StringBuilder eightBitNum, StringBuilder sixteenBitNum, char[] curGen){
+        String rule = intToBinary(intRule);
+
         while(true) {
-            String[] nextGen = new String[curGen.length];
+            int fourBitInt = binaryToInt(fourBitNum.toString());
+            int eightBitInt = binaryToInt(eightBitNum.toString());
 
-            for (int i = 0; i < curGen.length; i++) {
-                //setting next state for 0th index
-                if (i == 0) {
-                    nextGen[0] = getNextState(curGen[curGen.length-1] + curGen[0] + curGen[1]);
-                }
-                //setting next state for last index
-                else if (i == curGen.length - 1) {
-                    nextGen[curGen.length-1] = getNextState(curGen[i-1] + curGen[i] + curGen[0]);
-                }
-                //setting next state for middle indexes
-                else {
-                    nextGen[i] = getNextState(curGen[i-1] + curGen[i] + curGen[i+1]);
-                }
-            }
+            fourBitNum = rhythmDelay(fourBitNum, fourBitInt, curGen);
+            eightBitNum = melodicNote(eightBitNum, eightBitInt, curGen);
+            sixteenBitNum = droneNote(sixteenBitNum, fourBitInt, eightBitInt, curGen);
 
-            binaryNum += curGen[curGen.length/2];
-            if(binaryNum.length() == 4) binaryNum = binaryNum.substring(1);
-            System.out.println(Integer.parseInt(binaryNum, 2) * 250);
+            printArray(curGen);
 
-            try {
-                TimeUnit.MILLISECONDS.sleep(Integer.parseInt(binaryNum, 2) * 250);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-            /*
-            for (String cell : curGen) {
-                if(cell != null) {
-                    if (cell.equals("0")) System.out.print(".");
-                    else System.out.print("#");
-                }
-            }
-            System.out.println();
-            */
-            curGen = nextGen;
+            curGen = generate(rule, curGen);
         }
     }
 
-    public String getNextState(String neighborhood) {
-        if(neighborhood.equals("111")) return ""+rule.charAt(0);
-        else if(neighborhood.equals("110")) return ""+rule.charAt(1);
-        else if(neighborhood.equals("101")) return ""+rule.charAt(2);
-        else if(neighborhood.equals("100")) return ""+rule.charAt(3);
-        else if(neighborhood.equals("011")) return ""+rule.charAt(4);
-        else if(neighborhood.equals("010")) return ""+rule.charAt(5);
-        else if(neighborhood.equals("001")) return ""+rule.charAt(6);
-        else return ""+rule.charAt(7);
+    public char[] generate(String rule, char[] curGen) {
+        char[] nextGen = new char[curGen.length];
+        for (int i = 0; i < curGen.length; i++) {
+            //setting next state for 0th index
+            if (i == 0) {
+                nextGen[0] = getNextState(rule, curGen[curGen.length-1], curGen[0], curGen[1]);
+            }
+            //setting next state for last index
+            else if (i == curGen.length - 1) {
+                    nextGen[curGen.length-1] = getNextState(rule,curGen[i-1], curGen[i], curGen[0]);
+            }
+            //setting next state for middle indexes
+            else {
+                nextGen[i] = getNextState(rule,curGen[i-1], curGen[i], curGen[i+1]);
+            }
+        }
+        return nextGen;
     }
 
-    public static String getBinaryString(int val) {
-        if(val < 2) return ""+val;
-        String binary = "";
-        //converting int to reverse of binary String
-        while(val > 0) {
-            binary += "" + val % 2;
-            val = val / 2;
+    public char getNextState(String rule, char left, char at, char right) {//neighborhood refers to the two cells surrounding given index
+        if(left == '1' && at == '1' && right == '1') return rule.charAt(0);
+        else if(left == '1' && at == '1' && right == '0') return rule.charAt(1);
+        else if(left == '1' && at == '0' && right == '1') return rule.charAt(2);
+        else if(left == '1' && at == '0' && right == '0') return rule.charAt(3);
+        else if(left == '0' && at == '1' && right == '1') return rule.charAt(4);
+        else if(left == '0' && at == '1' && right == '0') return rule.charAt(5);
+        else if(left == '0' && at == '0' && right == '1') return rule.charAt(6);
+        else return rule.charAt(7);
+    }
+
+    public void printArray(char[] curGen) {
+        for (char cell : curGen) {
+            if (cell == '0') System.out.print(".");
+            else System.out.print("#");
+            }
+        System.out.println();
+    }
+
+    public StringBuilder rhythmDelay(StringBuilder fourBitNum, int fourBitInt, char[] curGen) {//uses 4 bit num to calculate delay
+        fourBitNum.append(curGen[curGen.length/2]);
+
+        if(fourBitNum.length() > 4) fourBitNum.deleteCharAt(0);//keeps number from becoming larger than 4 bits
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(fourBitInt * 50);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return fourBitNum;
+    }
+
+    public StringBuilder melodicNote(StringBuilder eightBitNum, int eightBitInt, char[] curGen) {
+        eightBitNum.append(curGen[curGen.length/2]);
+
+        Note.play(60 + eightBitInt % 14);
+
+        if(eightBitNum.length() > 8) eightBitNum.delete(0, 5);
+
+        return eightBitNum;
+    }
+    //drone note changes when last digit 4bit num = last digit 8bit num
+    public StringBuilder droneNote(StringBuilder sixteenBitNum, int fourBitInt, int eightBitInt, char[] curGen) {
+        sixteenBitNum.append(curGen[curGen.length/2]);
+
+        if(fourBitInt % 10 == eightBitInt % 10) Note.play(44 + binaryToInt(sixteenBitNum.toString()) % 14);
+
+        if(sixteenBitNum.length() > 16) sixteenBitNum.delete(0, 13);
+
+        return sixteenBitNum;
+    }
+
+    public static String intToBinary(int val) {
+        String binaryStr = "";
+        if(val < 2) binaryStr = ""+val;
+        else {
+            //converting int to reverse of binary String
+            while (val > 0) {
+                binaryStr += "" + val % 2;
+                val = val / 2;
+            }
         }
         //tacking on trailing 0's
-        for(int i = 8 - binary.length() ; i > 0; i--) {
-            binary += "0";
+        for(int i = 8 - binaryStr.length() ; i > 0; i--) {
+            binaryStr += "0";
         }
         //returning reverse of String
-        return new StringBuilder(binary).reverse().toString();
+        return new StringBuilder(binaryStr).reverse().toString();
+    }
+
+    public static int binaryToInt (String binaryStr) {
+        char[] numbers = binaryStr.toCharArray();
+        int result = 0;
+        for(int i=numbers.length - 1; i>=0; i--)
+            if(numbers[i]=='1')
+                result += Math.pow(2, (numbers.length-i - 1));
+        return result;
     }
 }
